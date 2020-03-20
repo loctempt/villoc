0a25c49f114acc7f434ab5217cd835d63304ff01
jxy <xiyuej@gmail.com>
2020-03-20 22:30:22 +0800

    用Misc类包装全局函数

--- villoc.py
+++ villoc.py
@@ -101,7 +101,7 @@ class Block(Printable):
     classes = Printable.classes + ["normal"]
 
     def __init__(self, addr, size, error=False, tmp=False, **kwargs):
-        self.color = kwargs.pop('color', random_color())
+        self.color = kwargs.pop('color', Misc.random_color())
         self.uaddr = addr
         self.usize = size
         self.details = True
@@ -169,371 +169,373 @@ def match_ptr(state, ptr):
         state.append(Marker(ptr, error=True))
 
     return s, smallest_match
+    
 
 
-def malloc(state, ret, size):
 
-    if not ret:
-        state.errors.append("Failed to allocate %#x bytes." % size)
-    else:
-        state.append(Block(ret, size))
+class Misc:
+    @staticmethod
+    def malloc(state, ret, size):
 
+        if not ret:
+            state.errors.append("Failed to allocate %#x bytes." % size)
+        else:
+            state.append(Block(ret, size))
 
-def calloc(state, ret, nmemb, size):
-    malloc(state, ret, nmemb * size)
+    @staticmethod
+    def calloc(state, ret, nmemb, size):
+        malloc(state, ret, nmemb * size)
 
+    @staticmethod
+    def free(state, ret, ptr):
 
-def free(state, ret, ptr):
+        if ptr is 0:
+            return
 
-    if ptr is 0:
-        return
+        s, match = match_ptr(state, ptr)
 
-    s, match = match_ptr(state, ptr)
+        if match is None:
+            return
+        elif ret is None:
+            state[s] = Block(
+                match.uaddr, match.usize, error=True, color=match.color
+            )
+        else:
+            del state[s]
 
-    if match is None:
-        return
-    elif ret is None:
-        state[s] = Block(
-            match.uaddr, match.usize, error=True, color=match.color
-        )
-    else:
-        del state[s]
+    @staticmethod
+    def realloc(state, ret, ptr, size):
 
+        if not ptr:
+            return malloc(state, ret, size)
+        elif not size:
+            return free(state, ret, ptr)
 
-def realloc(state, ret, ptr, size):
+        s, match = match_ptr(state, ptr)
 
-    if not ptr:
-        return malloc(state, ret, size)
-    elif not size:
-        return free(state, ret, ptr)
+        if match is None:
+            return
+        elif ret is None:
+            state[s] = Block(match.uaddr, match.usize, color=match.color, meta=match.meta)
+            state[s].error = True
+        else:
+            state[s] = Block(ret, size, color=match.color, meta=match.meta)
 
-    s, match = match_ptr(state, ptr)
-
-    if match is None:
-        return
-    elif ret is None:
-        state[s] = Block(match.uaddr, match.usize, color=match.color, meta=match.meta)
-        state[s].error = True
-    else:
-        state[s] = Block(ret, size, color=match.color, meta=match.meta)
-
-
-def meta(state, ret, *args):
-    msg = args[0]
-    anotations = args[1:]
-    return ([], ["after: %s, meta=%s" % (msg, anotations)], anotations)
-
-
-operations = {
-    'free': free,
-    'malloc': malloc,
-    'calloc': calloc,
-    'realloc': realloc,
-    '@villoc': meta,
-}
-
-
-def sanitize(x):
-    if x is None:
-        return None
-    x = x.strip()
-    if x == "<void>":
-        return 0
-    if x in ("(nil)", "nil"):
-        return 0
-    try:
-        return int(x, 0)
-    except:
-        return x
-
-
-def parse_ltrace(ltrace):
-
-    match_call = re.compile(r"^([@A-z_\.]+->)?([@A-z_]+)\((.*)\) += (.*)$")
-    match_err = re.compile(r"^([@A-z_\.]+->)?([@A-z_]+)\((.*) <no return \.\.\.>")
-
-    for line in ltrace:
-
-        # if the trace file contains PID (for ltrace -f)
-        head, _, tail = line.partition(" ")
-        if head.isdigit():
-            line = tail
-
+    @staticmethod
+    def meta(state, ret, *args):
+        msg = args[0]
+        anotations = args[1:]
+        return ([], ["after: %s, meta=%s" % (msg, anotations)], anotations)
+    
+    @staticmethod
+    def sanitize(x):
+        if x is None:
+            return None
+        x = x.strip()
+        if x == "<void>":
+            return 0
+        if x in ("(nil)", "nil"):
+            return 0
         try:
-            _, func, args, ret = match_call.findall(line)[0]
-            if not func in operations:
-                continue
-        except Exception:
+            return int(x, 0)
+        except:
+            return x
+
+    @staticmethod
+    def parse_ltrace(ltrace):
+
+        match_call = re.compile(r"^([@A-z_\.]+->)?([@A-z_]+)\((.*)\) += (.*)$")
+        match_err = re.compile(r"^([@A-z_\.]+->)?([@A-z_]+)\((.*) <no return \.\.\.>")
+
+        for line in ltrace:
+
+            # if the trace file contains PID (for ltrace -f)
+            head, _, tail = line.partition(" ")
+            if head.isdigit():
+                line = tail
 
             try:
-                # maybe this stopped the program
-                _, func, args = match_err.findall(line)[0]
+                _, func, args, ret = match_call.findall(line)[0]
                 if not func in operations:
                     continue
-                ret = None
             except Exception:
-                continue
 
-        args = list(map(sanitize, args.split(",")))
-        ret = sanitize(ret)
+                try:
+                    # maybe this stopped the program
+                    _, func, args = match_err.findall(line)[0]
+                    if not func in operations:
+                        continue
+                    ret = None
+                except Exception:
+                    continue
 
-        yield func, args, ret
+            args = list(map(Misc.sanitize, args.split(",")))
+            ret = Misc.sanitize(ret)
 
+            yield func, args, ret
 
-def build_timeline(events):
+    @staticmethod
+    def build_timeline(events):
 
-    boundaries = set()
-    timeline = [State()]
-    errors = []
-    info = []
-    meta = []
-
-    # FIXME: remove those accumulators, we should do this by keeping
-    # the same state from one loop to the other.
-
-    for func, args, ret in events:
-
-        try:
-            op = operations[func]
-        except KeyError:
-            continue
-
-        state = State(b for b in timeline[-1] if not b.tmp)
-        state.errors.extend(errors)
-        state.info.extend(info)
-        state.meta.extend(meta)
-
-        meta_info = op(state, ret, *args)
-        if meta_info:
-            # This was not an op but something meta.
-            # Add the info to accumulators and continue.
-            errors.extend(meta_info[0])
-            info.extend(meta_info[1])
-            meta.extend(meta_info[2])
-            continue
-
-        # This was an op and not something meta.
-        # Clean accumulators.
+        boundaries = set()
+        timeline = [State()]
         errors = []
         info = []
         meta = []
 
-        call = "%s(%s)" % (func, ", ".join("%#x" % a for a in args))
+        # FIXME: remove those accumulators, we should do this by keeping
+        # the same state from one loop to the other.
 
-        if ret is None:
-            state.errors.append("%s = <error>" % call)
-        else:
-            state.info.append("%s = %#x" % (call, ret))
+        for func, args, ret in events:
 
-        boundaries.update(state.boundaries())
-        timeline.append(state)
-
-    return timeline, boundaries
-
-
-def random_color(r=200, g=200, b=125):
-
-    red = (random.randrange(0, 256) + r) / 2
-    green = (random.randrange(0, 256) + g) / 2
-    blue = (random.randrange(0, 256) + b) / 2
-
-    return (red, green, blue)
-
-
-def print_state(out, boundaries, state):
-
-    out.write('<div class="state %s">\n' % ("error" if state.errors else ""))
-
-    known_stops = set()
-
-    todo = state
-    while todo:
-
-        out.write('<div class="line" style="">\n')
-
-        done = []
-
-        current = None
-        last = 0
-
-        for i, b in enumerate(boundaries):
-
-            # If this block has size 0; make it continue until the
-            # next boundary anyway. The size will be displayed as
-            # 0 or unknown anyway and it shouldn't be too confusing.
-            if current and current.end() != b and current.start() != current.end():
+            try:
+                op = operations[func]
+            except KeyError:
                 continue
 
-            if current:  # stops here.
-                known_stops.add(i)
-                current.gen_html(out, i - last)
-                done.append(current)
-                last = i
+            state = State(b for b in timeline[-1] if not b.tmp)
+            state.errors.extend(errors)
+            state.info.extend(info)
+            state.meta.extend(meta)
+
+            meta_info = op(state, ret, *args)
+            if meta_info:
+                # This was not an op but something meta.
+                # Add the info to accumulators and continue.
+                errors.extend(meta_info[0])
+                info.extend(meta_info[1])
+                meta.extend(meta_info[2])
+                continue
+
+            # This was an op and not something meta.
+            # Clean accumulators.
+            errors = []
+            info = []
+            meta = []
+
+            call = "%s(%s)" % (func, ", ".join("%#x" % a for a in args))
+
+            if ret is None:
+                state.errors.append("%s = <error>" % call)
+            else:
+                state.info.append("%s = %#x" % (call, ret))
+
+            boundaries.update(state.boundaries())
+            timeline.append(state)
+
+        return timeline, boundaries
+
+    @staticmethod
+    def random_color(r=200, g=200, b=125):
+
+        red = (random.randrange(0, 256) + r) / 2
+        green = (random.randrange(0, 256) + g) / 2
+        blue = (random.randrange(0, 256) + b) / 2
+
+        return (red, green, blue)
+
+    @staticmethod
+    def print_state(out, boundaries, state):
+
+        out.write('<div class="state %s">\n' % ("error" if state.errors else ""))
+
+        known_stops = set()
+
+        todo = state
+        while todo:
+
+            out.write('<div class="line" style="">\n')
+
+            done = []
 
             current = None
-            for block in todo:
-                if block.start() == b:
-                    current = block
-                    break
-            else:
-                continue
+            last = 0
 
-            if last != i:
+            for i, b in enumerate(boundaries):
 
-                # We want to show from previous known_stop.
+                # If this block has size 0; make it continue until the
+                # next boundary anyway. The size will be displayed as
+                # 0 or unknown anyway and it shouldn't be too confusing.
+                if current and current.end() != b and current.start() != current.end():
+                    continue
 
-                for s in reversed(range(last, i+1)):
-                    if s in known_stops:
-                        break
-
-                if s != last:
-                    Empty(boundaries[last], boundaries[s],
-                          display=False).gen_html(out, s - last)
-                    known_stops.add(s)
-
-                if s != i:
-                    Empty(boundaries[s], b).gen_html(out, i - s)
+                if current:  # stops here.
                     known_stops.add(i)
+                    current.gen_html(out, i - last)
+                    done.append(current)
+                    last = i
 
-                last = i
+                current = None
+                for block in todo:
+                    if block.start() == b:
+                        current = block
+                        break
+                else:
+                    continue
+
+                if last != i:
+
+                    # We want to show from previous known_stop.
+
+                    for s in reversed(range(last, i+1)):
+                        if s in known_stops:
+                            break
+
+                    if s != last:
+                        Empty(boundaries[last], boundaries[s],
+                              display=False).gen_html(out, s - last)
+                        known_stops.add(s)
+
+                    if s != i:
+                        Empty(boundaries[s], b).gen_html(out, i - s)
+                        known_stops.add(i)
+
+                    last = i
 
 
-        if current:
-            raise RuntimeError("Block was started but never finished.")
+            if current:
+                raise RuntimeError("Block was started but never finished.")
 
-        if not done:
-            raise RuntimeError("Some block(s) don't match boundaries.")
+            if not done:
+                raise RuntimeError("Some block(s) don't match boundaries.")
+
+            out.write('</div>\n')
+
+            todo = [x for x in todo if x not in done]
+
+        out.write('<div class="log">')
+
+        for msg in state.info:
+            out.write('<p>%s</p>' % html.escape(str(msg)))
+
+        for msg in state.errors:
+            out.write('<p>%s</p>' % html.escape(str(msg)))
 
         out.write('</div>\n')
 
-        todo = [x for x in todo if x not in done]
+        out.write('</div>\n')
 
-    out.write('<div class="log">')
+    @staticmethod
+    def gen_html(timeline, boundaries, out):
 
-    for msg in state.info:
-        out.write('<p>%s</p>' % html.escape(str(msg)))
+        if timeline and not timeline[0]:
+            timeline.pop(0)
 
-    for msg in state.errors:
-        out.write('<p>%s</p>' % html.escape(str(msg)))
+        boundaries = list(sorted(boundaries))
 
-    out.write('</div>\n')
+        out.write('<style>')
 
-    out.write('</div>\n')
+        out.write('''body {
+    font-size: 12px;
+    background-color: #EBEBEB;
+    font-family: "Lucida Console", Monaco, monospace;
+    width: %dem;
+    }
+    ''' % ((len(boundaries) - 1) * (Printable.unit_width + 1)))
 
+        out.write('''p {
+    margin: 0.8em 0 0 0.1em;
+    }
+    ''')
 
-def gen_html(timeline, boundaries, out):
+        out.write('''.block {
+    float: right;
+    padding: 0.5em 0;
+    text-align: center;
+    color: black;
+    }
+    ''')
 
-    if timeline and not timeline[0]:
-        timeline.pop(0)
+        out.write('''.normal {
 
-    boundaries = list(sorted(boundaries))
+    -webkit-box-shadow: 1px 2px 4px 0px rgba(0,0,0,0.80);
+    -moz-box-shadow: 1px 2px 4px 0px rgba(0,0,0,0.80);
+    box-shadow: 1px 2px 4px 0px rgba(0,0,0,0.80);
 
-    out.write('<style>')
+    }
+    ''')
 
-    out.write('''body {
-font-size: 12px;
-background-color: #EBEBEB;
-font-family: "Lucida Console", Monaco, monospace;
-width: %dem;
+        out.write('''.empty + .empty {
+    border-left: 1px solid gray;
+    margin-left: -1px;
+    }
+    ''')
+
+        out.write('''.empty {
+    color: gray;
+    }
+    ''')
+
+        out.write('.line {  }')
+
+        out.write('''.line:after {
+      content:"";
+      display:table;
+      clear:both;
+    }''')
+
+        out.write('''.state {
+    margin: 0.5em; padding: 0;
+
+    background-color: white;
+
+    border-radius: 0.3em;
+
+    -webkit-box-shadow: inset 2px 2px 4px 0px rgba(0,0,0,0.80);
+    -moz-box-shadow: inset 2px 2px 4px 0px rgba(0,0,0,0.80);
+    box-shadow: inset 2px 2px 4px 0px rgba(0,0,0,0.80);
+
+    padding: 0.5em;
+
+    }''')
+
+        out.write('''.log {
+    }''')
+
+        out.write('''.error {
+    color: white;
+    background-color: #8b1820;
+    }''')
+
+        out.write('''.error .empty {
+    color: white;
+    }''')
+
+        out.write('</style>\n')
+
+        out.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>')
+        out.write('''<script>
+    var scrollTimeout = null;
+    $(window).scroll(function(){
+        if (scrollTimeout) clearTimeout(scrollTimeout);
+        scrollTimeout = setTimeout(function(){
+        $('.log').stop();
+        $('.log').animate({
+            'margin-left': $(this).scrollLeft()
+        }, 100);
+        }, 200);
+    });
+    </script>
+    ''')
+
+        out.write('<body>\n')
+
+        out.write('<div class="timeline">\n')
+
+        for state in reversed(timeline):
+            Misc.print_state(out, boundaries, state)
+
+        out.write('</div>\n')
+
+        out.write('</body>\n')
+
+operations = {
+    'free': Misc.free,
+    'malloc': Misc.malloc,
+    'calloc': Misc.calloc,
+    'realloc': Misc.realloc,
+    '@villoc': Misc.meta,
 }
-''' % ((len(boundaries) - 1) * (Printable.unit_width + 1)))
-
-    out.write('''p {
-margin: 0.8em 0 0 0.1em;
-}
-''')
-
-    out.write('''.block {
-float: right;
-padding: 0.5em 0;
-text-align: center;
-color: black;
-}
-''')
-
-    out.write('''.normal {
-
--webkit-box-shadow: 1px 2px 4px 0px rgba(0,0,0,0.80);
--moz-box-shadow: 1px 2px 4px 0px rgba(0,0,0,0.80);
-box-shadow: 1px 2px 4px 0px rgba(0,0,0,0.80);
-
-}
-''')
-
-    out.write('''.empty + .empty {
-border-left: 1px solid gray;
-margin-left: -1px;
-}
-''')
-
-    out.write('''.empty {
-color: gray;
-}
-''')
-
-    out.write('.line {  }')
-
-    out.write('''.line:after {
-  content:"";
-  display:table;
-  clear:both;
-}''')
-
-    out.write('''.state {
-margin: 0.5em; padding: 0;
-
-background-color: white;
-
-border-radius: 0.3em;
-
--webkit-box-shadow: inset 2px 2px 4px 0px rgba(0,0,0,0.80);
--moz-box-shadow: inset 2px 2px 4px 0px rgba(0,0,0,0.80);
-box-shadow: inset 2px 2px 4px 0px rgba(0,0,0,0.80);
-
-padding: 0.5em;
-
-}''')
-
-    out.write('''.log {
-}''')
-
-    out.write('''.error {
-color: white;
-background-color: #8b1820;
-}''')
-
-    out.write('''.error .empty {
-color: white;
-}''')
-
-    out.write('</style>\n')
-
-    out.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>')
-    out.write('''<script>
-var scrollTimeout = null;
-$(window).scroll(function(){
-    if (scrollTimeout) clearTimeout(scrollTimeout);
-    scrollTimeout = setTimeout(function(){
-    $('.log').stop();
-    $('.log').animate({
-        'margin-left': $(this).scrollLeft()
-    }, 100);
-    }, 200);
-});
-</script>
-''')
-
-    out.write('<body>\n')
-
-    out.write('<div class="timeline">\n')
-
-    for state in reversed(timeline):
-        print_state(out, boundaries, state)
-
-    out.write('</div>\n')
-
-    out.write('</body>\n')
-
 
 if __name__ == '__main__':
 
@@ -573,6 +575,6 @@ if __name__ == '__main__':
 
 
     noerrors = codecs.getreader('utf8')(args.ltrace.detach(), errors='ignore')
-    timeline, boundaries = build_timeline(parse_ltrace(noerrors))
+    timeline, boundaries = Misc.build_timeline(Misc.parse_ltrace(noerrors))
 
-    gen_html(timeline, boundaries, args.out)
+    Misc.gen_html(timeline, boundaries, args.out)
